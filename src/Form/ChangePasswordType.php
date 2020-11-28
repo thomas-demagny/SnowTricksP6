b18987e1e08fa280eb66c5e67517ff273105d08e
Tom <demagny.t@hotmail.fr>
2020-11-28 14:09:33 +0100

    fix

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -22,7 +22,7 @@ class ChangePasswordType extends AbstractType
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('currentPassword', PasswordType::class, [
