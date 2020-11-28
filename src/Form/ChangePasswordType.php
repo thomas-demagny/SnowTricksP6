a01c91d29060ffc6071808af5ef5dc2a19924ce8
Tom <demagny.t@hotmail.fr>
2020-11-28 13:48:25 +0100

    add change password type

---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ * Class ChangePasswordType
+ * @package App\Form
+ */
+class ChangePasswordType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('currentPassword', PasswordType::class, [
+                'constraints'=> [
+                    new UserPassword(),
+                ],
+                'label' => 'mot de passe actuel',
+                'attr' => [
+                    'autocomplete' => 'off'
+                ],
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length( [
+                        'min' => 5,
+                        'max' => 50,
+                    ]),
+                ],
+                'first_options' => [
+                    'label' => 'nouveau mot de passe',
+                ],
+                'second_options'=>[
+                    'label' => 'confirmer nouveau mot de passe'
+                ],
+            ])
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
