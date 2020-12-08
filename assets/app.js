0aae25765f5a2868f82daed0b4f45a051402353c
Tom <demagny.t@hotmail.fr>
2020-12-08 11:49:42 +0100

    import forms and home css

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,11 @@
 import "./styles/app.css";
-//import "./styles/home.css";
+import "./styles/home.css"
+import "./styles/forms.css";
 
 const $ = require("jquery");
 require("bootstrap");
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 $(document).ready(function() {
     $("[data-toggle='popover']").popover();
