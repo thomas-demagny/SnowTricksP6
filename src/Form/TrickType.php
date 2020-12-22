2ba827d17236f379464a20783bb9777d56cdcb4d
Tom <demagny.t@hotmail.fr>
2020-12-22 17:48:56 +0100

    add trick formtype

---
+++ src/Form/TrickType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trick;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('createdAt')
+            ->add('updateAt')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+}
