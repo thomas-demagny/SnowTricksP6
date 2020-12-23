8ca650e36c5978b8b0fd2637cc2def37c0efffe7
Tom <demagny.t@hotmail.fr>
2020-12-23 12:01:38 +0100

    modified arrayCollection to collection to fix the issue

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -6,6 +6,8 @@ use App\Repository\TrickRepository;
 use DateTime;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
