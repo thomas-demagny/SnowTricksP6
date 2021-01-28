121c3a9c1649b0ba170eea61516df254444eb0b6
Tom <demagny.t@hotmail.fr>
2021-01-28 09:42:40 +0100

    add comments in trick

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -54,6 +54,11 @@ class Trick
     private Collection $categories;
 
     /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="trick", orphanRemoval=true)
+     */
+    private Collection $comments;
+
+    /**
      * Trick constructor.
      * @param UserInterface $user
      */
@@ -63,6 +68,7 @@ class Trick
         $this->setUpdateAt(new DateTime());
         $this->setUser($user);
         $this->categories = new ArrayCollection();
+        $this->comments = new ArrayCollection();
 
     }
 
@@ -191,6 +197,36 @@ class Trick
     {
         if ($this->categories->removeElement($category)) {
             $category->removeTrick($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getTrick() === $this) {
+                $comment->setTrick(null);
+            }
         }
 
         return $this;
