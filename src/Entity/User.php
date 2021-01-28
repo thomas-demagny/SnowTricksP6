bde916caddf9d4533df64c7e7961cc2afde7e9ab
Tom <demagny.t@hotmail.fr>
2021-01-28 09:43:35 +0100

    add comments in user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -67,12 +67,18 @@ class User implements UserInterface
     private Collection $trick;
 
     /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="user", orphanRemoval=true)
+     */
+    private Collection $comments;
+
+    /**
      * User constructor.
      */
     public function __construct()
     {
         $this->setCreatedAt(new DateTime());
         $this->trick = new ArrayCollection();
+        $this->comments = new ArrayCollection();
 
     }
     /**
@@ -272,6 +278,36 @@ class User implements UserInterface
             // set the owning side to null (unless already changed)
             if ($trick->getUser() === $this) {
                 $trick->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getUser() === $this) {
+                $comment->setUser(null);
             }
         }
 
