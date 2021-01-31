f66a7de4112f3cdfda35ace783b08185c129e05d
Tom <demagny.t@hotmail.fr>
2021-01-31 17:01:03 +0100

    I'm trying to fix this issue

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -2,79 +2,77 @@
 
 namespace App\DataFixtures;
 
+
+use App\Entity\Trick;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\User\User;
 
 
-class Trick extends Fixture implements DependentFixtureInterface
+/**
+ * Class TrickFixtures
+ * @package App\DataFixtures
+ */
+class TrickFixtures extends Fixture implements DependentFixtureInterface
 {
-
     /**
-     *
+     * Const int
      */
     CONST TRICKS_NUMBER = 20;
+    CONST TRICKS_NAME = [
+        '360',
+        'Air',
+        'FS 540',
+        '1080' ,
+        'Front Flip',
+        'Back Flip' ,
+        'Indy'
+    ];
 
     /**
      * @param ObjectManager $manager
      */
     public function load(ObjectManager $manager)
     {
-        $faker = Factory::create('fr_FR');
-        $categories = $this->shuffleCategories();
+        dd($this->getReference('user1'));
+        $usersData = $this->getReference('user'. mt_rand(1, UserFixtures::DATA));
 
-        for ($k = 0 ; $k < self::TRICKS_NUMBER; $k++) {
-            $date = $faker->dateTimeBetween('-10 days');
-            $trick = new \App\Entity\Trick($this->shuffleUsers()[$k]);
+        for ($k = 1 ; $k < self::TRICKS_NUMBER; $k++) {
 
-            $trick
-                ->setTitle($faker->text(50))
-                ->setDescription(join("\n", $faker->paragraphs(3)))
-                ->setCreatedAt($date)
-                ->setUpdateAt($date)
-            ;
-            $this->addReference("trick".$k, $trick);
+            $trick = $this->initTrick(new Trick($usersData[$k]));
+
             $manager->persist($trick);
+            $this->addReference("trick".$k, $trick);
         }
+
         $manager->flush();
     }
 
-    /**
-     * @return mixed
-     */
-    public function shuffleUsers()
+    public function initTrick( $trick): Trick
     {
-        for($i = 0; $i < self::TRICKS_NUMBER ; $i++) {
-            $usersData = 'user'.rand(0, UserFixtures::DATA);
-            $users[] = $this->getReference($usersData);
-        }
-        return $users;
-    }
+        $faker = Factory::create('fr_FR');
+        $date = $faker->dateTimeBetween('-2 months');
 
-    /**
-     * @return mixed
-     */
-    public function shuffleCategories()
-    {
-        for($j = 0; $j < self::TRICKS_NUMBER ; $j++) {
-            $categoriesData = new \App\Entity\Category();
-            $categoryTrick = [];
+        $userData = $this->getReference('toto');
+        $trick
+            ->setTitle($faker->randomElement(self::TRICKS_NAME))
+            ->setDescription(join("\n", $faker->paragraphs(3)))
+            ->setCreatedAt($date)
+            ->setUpdateAt($date)
+            ->setUser($userData);
 
-            for($i = 0; $i < rand(1,3); $i++) {
-                $key = array_rand($categoryTrick);
-                $categoriesTrick[] = $categoriesData[$key];
-            }
-            $categories[$j] = $categoriesTrick;
-        }
-        return $categories;
+        return $trick;
     }
 
     public function getDependencies(): array
     {
         return [
-            Category::class,
+            CategoryFixtures::class,
             UserFixtures::class
         ];
     }
-}
+
+
+}//end class
